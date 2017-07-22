0849ab8874367c40b017c3af5ba246ec796fed26
Kimia Hassanzadeh <khassanzadeh@intuit.com>
2017-07-21 21:07:45 -0600

    contact added

--- app.js
+++ app.js
@@ -25,6 +25,38 @@ app.get('/login', function(req,res){
 app.get('/about', function(req,res){
   res.render('about.html');
 });
+
+app.get('/contact', function(req,res){
+  res.render('contact.html');
+});
+
+app.post('/contact/send', function(req,res){
+  var transporter = nodemailer.createTransport({
+    service:"Gmail",
+    auth: {
+      user:'kim.hszd@gmail.com',
+      pass:'Stanlaurel9530!'
+    }
+  });
+  var mailOptions = {
+    from: 'B T',
+    to: 'khassanz@ualberta.ca',
+    subject:'boogh submission',
+    text:'You have a submission from Name:'+req.body.name+' Email:'+ req.body.email+ 'Message:'+req.body.message,
+    html: '<p>You have a submission with following details</p> <ul><li>from Name:'+req.body.name+'</li><li>Email:'+ req.body.email+ '</li><li>Message:'+req.body.message+'</li></ul>'
+  };
+  transporter.sendMail(mailOptions, function(error,info){
+      if(error){
+        console.log(error);
+        res.redirect('/');
+      }
+      else{
+        console.log('Message Sent');
+        res.redirect('/')
+      }
+  });
+});
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("Findmech server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
